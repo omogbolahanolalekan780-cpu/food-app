3edc1acce1a51fe75c1ccdfd3625674fe5deffc7
lekan <omogbolahanolalekan780@gmail.com>
2026-03-30 04:41:26 -0700

    create hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
      <Navbar />
-     
+     <Hero />
     </>
   )
 }
